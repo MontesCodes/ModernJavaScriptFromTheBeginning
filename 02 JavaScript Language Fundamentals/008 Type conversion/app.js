0baefb01cb2d90c0089ae623a43024bd056190b5
MontesCodes-CodeGaming <MontiesWebDev@gmail.com>
2022-09-18 17:52:16 -0400

    finished 008 Type Conversion, CodeGaming 9-18-2022

--- 02 JavaScript Language Fundamentals/008 Type conversion/app.js
+++ 02 JavaScript Language Fundamentals/008 Type conversion/app.js
@@ -1,12 +1,38 @@
 'use strict';
 let val;
-
+//Number to String
 val = String(555);
 
 val = String(444);
-
+// Bool to String
+val = String(true);
+val = String(false);
+// Date to String
 val = String(new Date());
+// Array to String
+val = String([1, 2, 3, 4]);
+// toString()
+val = (5).toString();
+val = true.toString();
+// String to number
+val = Number('8');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1, 2, 3]);
 
-console.log(val);
-console.log(typeof val);
-console.log(val.length);
+val = parseInt('100.30');
+val = parseFloat('100.30');
+
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.toFixed());
+// console.log(val.length);
+
+const val1 = String(5);
+const val2 = 6;
+const sum = Number(val1 + val2);
+
+console.log(sum);
+console.log(typeof sum);
