6d681d13b5e12cdde137afe24caca2f83040c91d
MontesCodes-CodeGaming <MontiesWebDev@gmail.com>
2022-09-18 20:07:19 -0400

    010 String methods Concatenation, CodeGaming 9-18-2022 #1

--- 02 JavaScript Language Fundamentals/010 String Methods  Concatenation/script.js
+++ 02 JavaScript Language Fundamentals/010 String Methods  Concatenation/script.js
@@ -1,1 +1,17 @@
 'use strict';
+const firstName = 'William';
+const lastName = 'Johnson';
+
+let val;
+
+val = firstName + lastName;
+
+// Concatenation
+
+val = firstName + ' ' + lastName;
+
+// Append
+val = 'Montes ';
+val += 'Codes';
+
+console.log(val);
