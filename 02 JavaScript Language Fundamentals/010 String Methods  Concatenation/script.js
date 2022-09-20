d688f3a028a417d9bf2b58f7886a69bdfbaf8fb0
MontesCodes-CodeGaming <MontiesWebDev@gmail.com>
2022-09-19 21:12:09 -0400

    updating 010 String Methods Concatenation, GamingDesk 9/19/2022 #1

--- 02 JavaScript Language Fundamentals/010 String Methods  Concatenation/script.js
+++ 02 JavaScript Language Fundamentals/010 String Methods  Concatenation/script.js
@@ -1,6 +1,9 @@
 'use strict';
 const firstName = 'William';
 const lastName = 'Johnson';
+const age = 38;
+const str = 'Hello there I am whom you think that I am';
+const tags = 'Web Design, Web Development, programming';
 
 let val;
 
@@ -13,5 +16,41 @@ val = firstName + ' ' + lastName;
 // Append
 val = 'Montes ';
 val += 'Codes';
+
+val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+
+// Escaping
+val = "that's awesome, I can't wait";
+
+// Length
+val = firstName.length;
+
+// Concat()
+val = firstName.concat(' ', lastName);
+
+// Change Case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[2];
+
+// indexOf()
+val = firstName.indexOf('1');
+val = firstName.lastIndexOf('1');
+
+// charAt()
+val = firstName.charAt('2');
+//Get last char
+val = firstName.charAt(firstName.length - 1);
+
+//substring()
+val = firstName.substring(0, 4);
+
+//  slice()
+val = firstName.slice(-3);
+
+// split
+val = str.split(' ');
+val = tags.split(',');
 
 console.log(val);
