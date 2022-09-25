d3d73c7d2a32819811d39b77b2746decb9bda7a1
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-09-25 19:00:48 -0400

    finishing 013 Object Literals, MacBookPro 9/25/2022

--- 02 JavaScript Language Fundamentals/013 Object Literals/app.js
+++ 02 JavaScript Language Fundamentals/013 Object Literals/app.js
@@ -27,3 +27,13 @@ val = person.address['city'];
 val = person.getBirthYear();
 
 console.log(val);
+
+const people = [
+  { name: 'John', age: 30 },
+  { name: 'Mike', age: 23 },
+  { name: 'Nancy', age: 40 },
+];
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].name);
+}
