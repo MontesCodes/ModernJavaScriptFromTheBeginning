155ebed35deb40d2fb9cb30379b9729023d9e101
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-09-25 19:36:48 -0400

    updating 015 if Statements Comparison Operators #2, MacBookPro 9/25/2022

--- 02 JavaScript Language Fundamentals/015 If Statements  Comparison Operators/app.js
+++ 02 JavaScript Language Fundamentals/015 If Statements  Comparison Operators/app.js
@@ -1,9 +1,38 @@
 'use strict';
 const id = 100;
+const id2 = 100;
 
 // Equal to
 if (id == 100) {
   console.log('Correct');
 } else {
   console.log('Incorrect');
+}
+
+// Not Equal To
+if (id != 101) {
+  console.log('Correct');
+} else {
+  console.log('INCorrect');
+}
+
+// Equal to Value & Type
+if (id === 100) {
+  console.log('Correct');
+} else {
+  console.log('Incorrect');
+}
+
+// Not Equal to Value & Type
+if (id !== 100) {
+  console.log('Correct');
+} else {
+  console.log('Incorrect');
+}
+
+// Test if undefined
+if (typeof id2 !== 'undefined') {
+  console.log(`The ID is ${id2}`);
+} else {
+  console.log('No ID');
 }
