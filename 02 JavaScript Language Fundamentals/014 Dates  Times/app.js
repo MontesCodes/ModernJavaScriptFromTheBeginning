96807d15dcbeaf15dc9618db502b7ccdad3dc8e5
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-09-25 19:13:17 -0400

    finishing 014 Dates Times, MacBookPro 9/25/2022

--- 02 JavaScript Language Fundamentals/014 Dates  Times/app.js
+++ 02 JavaScript Language Fundamentals/014 Dates  Times/app.js
@@ -3,8 +3,8 @@ let val;
 
 const today = new Date();
 const birthday = new Date('9-10-1981 11:25:00');
-let birthday1 = new Date('9-10-1981 11:25:00');
-const birthday2 = new Date('9-10-1981 11:25:00');
+let birthday1 = new Date('September 10 1981');
+const birthday2 = new Date('9/10/1981');
 
 val = today;
 val = today.getMonth();
@@ -23,7 +23,12 @@ birthday.setFullYear(1985);
 birthday.setHours(3);
 birthday.setMinutes(33);
 birthday.setSeconds(27);
+birthday1.setMonth(3);
+birthday2.setFullYear(1977);
 
 console.log(birthday);
+console.log(birthday1);
+console.log(birthday2);
 
 console.log(val);
+console.log(typeof val);
