05a6e30411d61e4a65bf338ebaab4ad2731e8d00
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-09-26 20:09:57 -0400

    finished 015 If Statements Comparison Operators, MacBookPro 9/25/2022

--- 02 JavaScript Language Fundamentals/015 If Statements  Comparison Operators/app.js
+++ 02 JavaScript Language Fundamentals/015 If Statements  Comparison Operators/app.js
@@ -44,3 +44,43 @@ if (id3 > 200) {
 } else {
   console.log('Incorrect');
 }
+
+const color = 'red';
+
+if (color === 'red') {
+  console.log('Color is red');
+} else if (color === 'blue') {
+  console.log('Color is blue');
+} else {
+  console.log('Color is not red or blue');
+}
+
+// Logical Operators
+
+const personName = 'Steve';
+const age = 20;
+
+if (age > 0 && age < 12) {
+  console.log(`${personName} is a child`);
+} else if (age >= 13 && age <= 19) {
+  console.log(`${personName} is a teenager`);
+} else {
+  console.log(`${personName} is an adult`);
+}
+
+// OR ||
+if (age < 16 || age > 65) {
+  console.log(`${personName} can not run in race`);
+} else {
+  console.log(`${personName} is registered for the race`);
+}
+
+// Ternary Operator
+console.log(id === 100 ? 'Correct' : 'Incorrect');
+
+// Without Braces
+if (id === 100) console.log(`correct`);
+else console.log(`Incorrect`);
+
+if (id2 === 100) console.log(`correct`);
+else console.log(`Incorrect`);
