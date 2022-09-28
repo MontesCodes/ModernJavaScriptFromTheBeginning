9a7bcc19727ac9afcebfe56e47a3a7453ee066de
MontesCodes-MacBookProStation <montieswebdev@gmail.com>
2022-09-28 18:42:39 -0400

    updating 017 Function Declarations, MacBookProDesktop 9/28/22 #1

--- 02 JavaScript Language Fundamentals/017 Function Declarations  Expressions/app.js
+++ 02 JavaScript Language Fundamentals/017 Function Declarations  Expressions/app.js
@@ -1,15 +1,33 @@
 'use strict';
 // FUNCTION DECLARATIONS
 
-function greet(firstName, lastName) {
-  if (typeof firstName === ' undefined') {
+function greet(firstName = 'John', lastName = 'Doe') {
+  /* if (typeof firstName === ' undefined') {
     firstName = 'John';
   }
   if (typeof lastName === ' undefined') {
     lastName = 'Doe';
-  }
+  } */
   // console.log('Hello');
   return 'Hello ' + firstName + ' ' + lastName;
 }
 
-console.log(greet('Steve', 'Smith'));
+// console.log(greet('Steve', 'Smith'));
+
+// FUNCTION EXPRESIONS
+
+const square = function (x = 3) {
+  return x * x;
+};
+
+// console.log(square());
+
+// IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS -IIFEs
+
+/* (function () {
+  console.log('IIFE ran...');
+})(); */
+
+(function (name1) {
+  console.log('Hello ' + name1);
+})('Montes');
