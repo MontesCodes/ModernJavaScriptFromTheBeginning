2da4b7a5f04d09d459d8b95505acf2c8ddf60004
MontesCodes-MacBookProStation <montieswebdev@gmail.com>
2022-09-28 20:31:33 -0400

    updating 018 General Loops, MacBookProDesktop 9/28/22 #2

--- 02 JavaScript Language Fundamentals/018 General Loops/app.js
+++ 02 JavaScript Language Fundamentals/018 General Loops/app.js
@@ -1,6 +1,42 @@
 'use strict';
 // FOR LOOP
 
-for (let i = 0; i < 10; i++) {
-  console.log('Number ' + i);
-}
+// for (let i = 0; i < 10; i++) {
+//   if (i === 2) {
+//     console.log('2 is my favorite number');
+//     continue;
+//   }
+//   if (i === 5) {
+//     console.log('stop the loop');
+//     break;
+//   }
+//   console.log('Number ' + i);
+// }
+
+// WHILE LOOP
+
+// let i = 0;
+
+// while (i < 10) {
+//   console.log('Number ' + i);
+//   i++;
+// }
+
+// DO WHILE
+
+// let i = 0;
+
+// do {
+//   console.log('Number ' + i);
+//   i++;
+// } while (i < 10);
+
+const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+// for (let i = 0; i < cars.length; i++) {
+//   console.log(cars[i]);
+// }
+
+cars.forEach(function (car) {
+  console.log(car);
+});
