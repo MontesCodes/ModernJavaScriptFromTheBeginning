099256edc7639cd6bdf0f74ef280b9b550e06767
MontesCodes-MacBookProStation <montieswebdev@gmail.com>
2022-09-28 20:44:51 -0400

    finished 018 General Loops, MacBookProDesktop 9/28/22

--- 02 JavaScript Language Fundamentals/018 General Loops/app.js
+++ 02 JavaScript Language Fundamentals/018 General Loops/app.js
@@ -31,12 +31,43 @@
 //   i++;
 // } while (i < 10);
 
+// LOOP THROUGH ARRAY
 const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
 
 // for (let i = 0; i < cars.length; i++) {
 //   console.log(cars[i]);
 // }
 
-cars.forEach(function (car) {
-  console.log(car);
-});
+// FOREACH
+// cars.forEach(function (car) {
+//   console.log(car);
+// });
+
+// cars.forEach(function (car, index, array) {
+//   console.log(`${index} : ${car}`);
+//   console.log(array);
+// });
+
+// MAP
+// const users = [
+//   { id: 1, name: 'John' },
+//   { id: 2, name: 'Sara' },
+//   { id: 3, name: 'Karen' },
+// ];
+
+// const ids = users.map(function (user) {
+//   return user.id;
+// });
+
+// console.log(ids);
+
+// FOR IN LOOP
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 40,
+};
+
+for (let x in user) {
+  console.log(`${x} : ${user[x]}`);
+}
