d29516056a08503535492683d853fae96fa2cf69
MontesCodes-MacBookProStation <montieswebdev@gmail.com>
2022-10-02 16:50:06 -0400

    updating 019 A Look At The Window Object, MacBookProDesktop 10/01/22

--- 02 JavaScript Language Fundamentals/019 A Look At The Window Object/app.js
+++ 02 JavaScript Language Fundamentals/019 A Look At The Window Object/app.js
@@ -1,7 +1,18 @@
 'use strict';
 // WINDOW METHODS / OBJECTS / PROPERTIES
 
-console.log(123);
+// console.log(123);
 
 // ALERT
 // alert('Hello World');
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+if (confirm('Are you sure')) {
+  console.log('YES');
+} else {
+  console.log('NO');
+}
