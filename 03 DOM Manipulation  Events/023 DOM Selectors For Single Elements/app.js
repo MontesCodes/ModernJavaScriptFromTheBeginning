58c8ed9e096535ff24cd96be6ecab48de63fdff2
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-10-23 17:32:26 -0400

    updating 023 DOM Selectors For Single Elements, MacBookPro 10/23/2022 #2

--- 03 DOM Manipulation  Events/023 DOM Selectors For Single Elements/app.js
+++ 03 DOM Manipulation  Events/023 DOM Selectors For Single Elements/app.js
@@ -1,1 +1,17 @@
 'use strict';
+// document.getElementById()
+
+console.log(document.getElementById('task-title'));
+
+/// Get Things from the element
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
+
+/// Change Styling
+document.getElementById('task-title').style.background = '#333';
+document.getElementById('task-title').style.color = '#fff';
+document.getElementById('task-title').style.padding = '5px';
+// document.getElementById('task-title').style.display = 'none';
+
+/// Change Content
+document.getElementById('task-title').textContent = 'Task List';
